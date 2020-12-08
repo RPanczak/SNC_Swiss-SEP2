bd8988c17c9c9f949289b421bf572314f1dd0d6a
RPanczak <r.panczak@gmail.com>
2020-12-08 22:58:09 +0100

    not needed

--- R/02_convert-addresses-sav-dta.R
+++
@@ -1,4 +0,0 @@
-library(haven)
-adresses_13 <- read_sav("./Stata/orig/SHP/SHPaddresses/adresses_13.sav")
-View(adresses_13)
-write_dta(adresses_13, "./Stata/orig/SHP/SHPaddresses/adresses_13.dta", version = 14)
