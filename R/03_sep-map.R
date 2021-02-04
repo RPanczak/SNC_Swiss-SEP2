751231d75bf4bb9133eee3128defd4927b4b90b4
RPanczak <r.panczak@gmail.com>
2021-02-04 13:11:08 +0100

    ispm_geo as source loc

--- R/03_sep-map.R
+++ R/03_sep-map.R
@@ -3,11 +3,11 @@ library(sf)
 library(ggplot2)
 library(RColorBrewer)
 
-canton <- st_read("data-raw/ag-b-00.03-875-gg20/ggg_2020-LV03/shp/g1k20.shp")
+canton <- st_read("../ISPM_geo/data-raw/ag-b-00.03-875-gg20/ggg_2020-LV03/shp/g1k20.shp")
 
-lake <- st_read("data-raw/ag-b-00.03-875-gg20/ggg_2020-LV03/shp/g1s20.shp")
+lake <- st_read("../ISPM_geo/data-raw/ag-b-00.03-875-gg20/ggg_2020-LV03/shp/g1s20.shp")
 
-ssep_user_geo <- readRDS("data/Swiss-SEP2/ssep3_user_geo.Rds") %>% 
+ssep3_user_geo <- readRDS("FINAL/RDS/ssep3_user_geo.Rds") %>% 
   mutate(ssep3_d = factor(ssep3_d,
                           levels = 1:10,
                           labels = c("1st - lowest", 
@@ -17,7 +17,7 @@ ssep_user_geo <- readRDS("data/Swiss-SEP2/ssep3_user_geo.Rds") %>%
                                      "10th - highest")))
 
 # for experiments
-ssep_user_geo_samp <- ssep_user_geo %>% 
+ssep3_user_geo_samp <- ssep3_user_geo %>% 
   sample_frac(0.01)
 
 # display.brewer.pal(n = 10, name = "RdYlGn") 
@@ -30,7 +30,7 @@ plot(st_geometry(lake),
      # col = rgb(0, 135, 208, max = 255), border = NA, 
      col = rgb(156, 213, 248, max = 255), border = NA, 
      add = TRUE)
-plot(ssep_user_geo_samp["ssep3_d"], 
+plot(ssep3_user_geo_samp["ssep3_d"], 
      pal = brewer.pal(n = 10, name = "RdYlGn"), 
      cex = 0.1, pch = 16, 
      add = TRUE)
@@ -39,8 +39,8 @@ plot(ssep_user_geo_samp["ssep3_d"],
 ggplot() + 
   geom_sf(data = lake, color = NA,  fill = rgb(156, 213, 248, max = 255)) + 
   geom_sf(data = canton, fill = NA, color = gray(.5), size = 0.25) +
-  geom_sf(data = ssep_user_geo, aes(colour = ssep3_d), alpha = 0.66, shape=".", size = 0.1) +
-  # geom_sf(data = ssep_user_geo_samp, aes(colour = ssep3_d), alpha = 0.66, shape=".", size = 0.1) +
+  # geom_sf(data = ssep3_user_geo, aes(colour = ssep3_d), alpha = 0.66, shape=".", size = 0.1) +
+  geom_sf(data = ssep3_user_geo_samp, aes(colour = ssep3_d), alpha = 0.66, shape=".", size = 0.1) +
   scale_colour_brewer(palette = "RdYlGn") +
   theme_void() + 
   guides(colour = guide_legend(title = "Swiss-SEP",
