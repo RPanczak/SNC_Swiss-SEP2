0eb78018da5b0c550823856034d22a162d53c0b9
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-04-28 15:14:16 +0200

    fixed paths and carto on deciles
    instead of mean and median

--- R/05_carto.R
+++ R/05_carto.R
@@ -23,6 +23,7 @@ View(statpop18)
 
 gem <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg18/ggg_2018-LV95/shp/g1g18.shp",
                options = "ENCODING=WINDOWS-1252") %>%
+  st_transform(2056) %>% 
   select(GMDNR, GMDNAME ) %>%
   left_join(statpop18) %>%
   filter(!is.na(B18BTOT))
@@ -30,19 +31,19 @@ gem <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg18/ggg_2018-LV95/shp/g
 str(gem)
 View(st_drop_geometry(gem))
 
-st_write(gem, "data/BfS/ag-b-00.03-875-gg18/gem-statpop-18.shp")
-
-lake <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg18/ggg_2018-LV95/shp/g1s18.shp",
-                options = "ENCODING=WINDOWS-1252") %>%
-  select(SEENAME)
-
-st_write(lake, "data/BfS/ag-b-00.03-875-gg18/lake.shp")
-
-canton <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg20/ggg_2020-LV95/shp/g1k20.shp",
-                  options = "ENCODING=WINDOWS-1252") %>%
-  select(KTNAME)
-
-st_write(canton, "data/BfS/ag-b-00.03-875-gg18/canton.shp")
+# st_write(gem, "data/BfS/ag-b-00.03-875-gg18/gem-statpop-18.shp")
+# 
+# lake <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg18/ggg_2018-LV95/shp/g1s18.shp",
+#                 options = "ENCODING=WINDOWS-1252") %>%
+#   select(SEENAME)
+# 
+# st_write(lake, "data/BfS/ag-b-00.03-875-gg18/lake.shp")
+# 
+# canton <- st_read("../ISPM_geo/data-raw/BfS/ag-b-00.03-875-gg20/ggg_2020-LV95/shp/g1k20.shp",
+#                   options = "ENCODING=WINDOWS-1252") %>%
+#   select(KTNAME)
+# 
+# st_write(canton, "data/BfS/ag-b-00.03-875-gg18/canton.shp")
 
 # #################################################
 # java -Xmx1024m -jar ../ISPM_geo/software/ScapeToad-v11/ScapeToad.jar
@@ -61,8 +62,8 @@ lake_carto <- st_read("data/BfS/ag-b-00.03-875-gg18/lake-carto.shp",
 canton_carto <- st_read("data/BfS/ag-b-00.03-875-gg18/canton-carto.shp",
                         options = "ENCODING=WINDOWS-1252")
 
-plot(st_geometry(gem_carto))
-plot(st_geometry(lake_carto), add = TRUE, col = "lightblue", border = NA)
+plot(st_geometry(lake_carto), col = "lightblue", border = NA)
+plot(st_geometry(gem_carto), add = TRUE, col = NA, border = "grey40")
 
 plot(st_geometry(lake_carto), col = "lightblue", border = NA)
 plot(st_geometry(canton_carto), add = TRUE, col = NA, border = "grey40")
@@ -72,12 +73,9 @@ plot(st_geometry(canton_carto), add = TRUE, col = NA, border = "grey40")
 
 ssep3_user_geo <- readRDS("FINAL/RDS/ssep3_user_geo.Rds") 
 
-gem18 <- readRDS("../ISPM_geo/data/swisstopo/swissBOUNDARIES3D/gem18.Rds") %>% 
-  st_transform(2056)
-
 # overlay
-ssep3_gem_18_sum <- 
-  st_join(ssep3_user_geo, gem18, join = st_intersects) %>% 
+ssep3_gem_18_sum <-  
+  st_join(ssep3_user_geo, gem, join = st_intersects) %>% 
   st_drop_geometry() %>% 
   group_by(GMDNR) %>% 
   summarise(ssep3_mean = mean(ssep3),
@@ -89,7 +87,8 @@ ssep3_gem_18_carto <- gem_carto %>%
 
 ggplot() + 
   geom_sf(data = lake_carto, color = NA,  fill = rgb(156, 213, 248, max = 255)) + 
-  geom_sf(data = ssep3_gem_18_carto, aes(fill = ssep3_median), 
+  geom_sf(data = ssep3_gem_18_carto, 
+          aes(fill = ssep3_median), 
           colour = "white",
           size = 0.1) +
   geom_sf(data = canton_carto, fill = NA, color = gray(.5), size = 0.1) +
@@ -99,13 +98,14 @@ ggplot() +
                                override.aes = list(alpha = 1, shape = 16, size = 3)))
 
 
-ggsave("carto/03_cartogram-r-median.png", 
-       width = 297, height = 210, units = "mm", dpi = 300)
+# ggsave("carto/03_cartogram-r-median.png", 
+#        width = 297, height = 210, units = "mm", dpi = 300)
 
 
 ggplot() + 
   geom_sf(data = lake_carto, color = NA,  fill = rgb(156, 213, 248, max = 255)) + 
-  geom_sf(data = ssep3_gem_18_carto, aes(fill = ssep3_mean), 
+  geom_sf(data = ssep3_gem_18_carto, 
+          aes(fill = ssep3_mean), 
           colour = "white",
           size = 0.1) +
   geom_sf(data = canton_carto, fill = NA, color = gray(.5), size = 0.1) +
@@ -115,15 +115,31 @@ ggplot() +
                                override.aes = list(alpha = 1, shape = 16, size = 3)))
 
 
-ggsave("carto/03_cartogram-r-mean.png", 
+# ggsave("carto/03_cartogram-r-mean.png", 
+#        width = 297, height = 210, units = "mm", dpi = 300)
+
+
+ggplot() + 
+  geom_sf(data = lake_carto, color = NA,  fill = rgb(156, 213, 248, max = 255)) + 
+  geom_sf(data = ssep3_gem_18_carto, 
+          aes(fill = factor(decile)), 
+          colour = NA,
+          alpha = 0.8) +
+  geom_sf(data = canton_carto, fill = NA, color = gray(.5), size = 0.1) +
+  scale_fill_brewer(palette = "RdYlGn", direction = 1) +
+  theme_void() + 
+  guides(colour = guide_legend(title = "Swiss-SEP",
+                               override.aes = list(alpha = 1, shape = 16, size = 3)))
+
+
+ggsave("carto/03_cartogram-r-decile.png", 
        width = 297, height = 210, units = "mm", dpi = 300)
 
 # for hex on website
 ggplot() + 
   geom_sf(data = lake_carto, color = NA,  fill = rgb(156, 213, 248, max = 255)) + 
   geom_sf(data = ssep3_gem_18_carto, aes(fill = factor(decile)), 
-          colour = NA,
-          size = 0.1) +
+          colour = NA) +
   geom_sf(data = canton_carto, fill = NA, color = "white", size = 0.1) +
   scale_fill_brewer(palette = "PuOr", direction = 1) +
   theme_void() + theme(legend.position = "none") 
