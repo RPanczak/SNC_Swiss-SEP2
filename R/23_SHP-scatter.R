bd3dd3ff2b2eecb6e769e4dc3b808104d61384d8
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-18 17:09:46 +0200

    testing alternatives

--- R/23_SHP-scatter.R
+++ R/23_SHP-scatter.R
@@ -1,10 +1,11 @@
 library(pacman)
-p_load(tidyverse, haven)
-
+p_load(tidyverse, haven, ggridges, hrbrthemes)
 
 SHP_income <- read_dta("data/SHP_income.dta") %>%
-  zap_label() %>%
-  zap_labels()
+  zap_label() %>% zap_labels() %>% 
+  mutate(ssep1_d = factor(ssep1_d),
+         ssep2_d = factor(ssep2_d),
+         ssep3_d = factor(ssep3_d))
 
 # full dataset
 ggplot(SHP_income, aes(x = ssep3, y = eq_ihtyni / 10000)) +
@@ -26,3 +27,21 @@ ggplot(SHP_income, aes(x = ssep3, y = eq_ihtyni / 10000)) +
   theme_minimal() +
   theme(aspect.ratio = 1)
 
+# distributions
+ggplot(SHP_income, aes(x = eq_ihtyni / 10000, weight = wh14css)) +
+  geom_density(aes(color = ssep3_d)) +
+  scale_colour_brewer(palette = "RdYlGn") +
+  ylab("Equivalised income [in 10k CHF]") +
+  xlab("Swiss-SEP3") +
+  theme_minimal() 
+
+# ggridges 
+ggplot(SHP_income,
+       aes(x = eq_ihtyni / 10000, 
+           y = ssep3_d, 
+           fill = ssep3_d)) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  coord_cartesian(xlim = c(NA, 30)) +
+  scale_fill_brewer(palette = "RdYlGn", name = "Index decile") +
+  theme_ipsum_rc() +
+  ylab("Swiss-SEP index decile") + xlab("Equivalence income")
